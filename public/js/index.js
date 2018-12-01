e6eba90d3f1e3163205abc39f86fa353b9a556a5
Ian Dunkelberg <idunkelberg@gmail.com>
2018-11-30 17:20:46 -0800

    Working on G+ and twitter account verification.

--- public/js/index.js
+++ public/js/index.js
@@ -87,6 +87,7 @@ var handleFormSubmit = function(event) {
 
 //LOGIN//
 
+//Log in with Facebook//
 $('#facebook-button').on('click', function() {
   // Initialize with your OAuth.io app public key
   OAuth.initialize('vjlnKXXv_pB-M71yxzZp5Z5hB-k');
@@ -106,4 +107,26 @@ $('#facebook-button').on('click', function() {
       console.log('self data:', data);
     })
   });
-})
+});
+//Log in with google//
+$('.fa-google-plus-square').on('click', function() {
+  // console.log("google clicked");
+  // // Initialize with your OAuth.io app public key
+  // OAuth.initialize('vjlnKXXv_pB-M71yxzZp5Z5hB-k');
+  // // Use popup for oauth
+  // OAuth.popup('google').then(google => {
+  //   console.log('google:',google);
+  //   // Prompts 'welcome' message with User's email on successful login
+  //   // #me() is a convenient method to retrieve user data without requiring you
+  //   // to know which OAuth provider url to call
+  //   google.me().then(data => {
+  //     console.log('me data:', data);
+  //     alert('Google says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
+  //   })
+    // Retrieves user data from OAuth provider by using #get() and
+    // OAuth provider url
+    // google.get('/v2.5/me?fields=name,first_name,last_name,email,gender,location,locale,work,languages,birthday,relationship_status,hometown,picture').then(data => {
+    //   console.log('self data:', data);
+    // })
+  //});
+});
