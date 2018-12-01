c22d0cdd52a24dc0ca92399b22f40a0aa9fbac49
Ian Dunkelberg <dunkeltron@users.noreply.github.com>
2018-12-01 10:32:53 -0800

    delete keys.js

--- keys.js
+++
@@ -1,5 +0,0 @@
-module.exports.oAuthKeys ={
-  id: "80481251698-gjc9859qica9mdfdp58qo1dnofa0tjmn.apps.googleusercontent.com",
-  secret: "Buf9mfJiMoTOjwVE3QUrWcGY",
-  callbackURL: "http://localhost:3000/auth/google/callback"
-};
