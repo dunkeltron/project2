959ff9f5033749a5f6e86ca0762789ecbb6d5a40
Ian Dunkelberg <idunkelberg@gmail.com>
2018-11-30 20:09:05 -0800

    added support for login with facebook, twitter, and google+

---
+++ models/user.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes){
+    var User = sequelize.define("User", {
+        userName :{
+            type: DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                len:[1]
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull:true,
+            validate: {
+                len: [1]
+            }
+        }
+    });
+    return User;
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,132 +1,184 @@
+$(document).ready(function () {
+  // Get references to page elements
+  var $exampleText = $("#example-text");
+  var $exampleDescription = $("#example-description");
+  var $submitBtn = $("#submit");
+  var $exampleList = $("#example-list");
+  var newUser;
 
-
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  // The API object contains methods for each kind of request we'll make
+  var API = {
+    saveExample: function (example) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/user",
+        data: JSON.stringify(example)
+      });
+    },
+    getExamples: function () {
+      return $.ajax({
+        url: "api/user",
+        type: "GET"
+      });
+    },
+    deleteExample: function (id) {
+      return $.ajax({
+        url: "api/user/" + id,
+        type: "DELETE"
+      });
+    }
   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
+  // refreshExamples gets new examples from the db and repopulates the list
+  var refreshExamples = function () {
+    API.getExamples().then(function (data) {
+      var $examples = data.map(function (example) {
+        var $a = $("<a>")
+          .text(example.text)
+          .attr("href", "/example/" + example.id);
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
+        var $li = $("<li>")
+          .attr({
+            class: "list-group-item",
+            "data-id": example.id
+          })
+          .append($a);
+
+        var $button = $("<button>")
+          .addClass("btn btn-danger float-right delete")
+          .text("ｘ");
+
+        $li.append($button);
+
+        return $li;
+      });
+
+      $exampleList.empty();
+      $exampleList.append($examples);
+    });
+  };
+
+  // handleFormSubmit is called whenever we submit a new example
+  // Save the new example to the db and refresh the list
+  var handleFormSubmit = function (event) {
+    event.preventDefault();
+
+    var example = {
+      text: $exampleText.val().trim(),
+      description: $exampleDescription.val().trim()
+    };
+
+    if (!(example.text && example.description)) {
+      alert("You must enter an example text and description!");
+      return;
+    }
+
+    API.saveExample(example).then(function () {
+      refreshExamples();
+    });
+
+    $exampleText.val("");
+    $exampleDescription.val("");
+  };
+
+
+  //LOGIN//
+
+  //Log in with Facebook//
+  $('#facebook-button').on('click', function () {
+    // Initialize with your OAuth.io app public key
+    OAuth.initialize('vjlnKXXv_pB-M71yxzZp5Z5hB-k');
+    // Use popup for oauth
+    OAuth.popup('facebook').then(facebook => {
+      console.log('facebook:', facebook);
+      // Prompts 'welcome' message with User's email on successful login
+      // #me() is a convenient method to retrieve user data without requiring you
+      // to know which OAuth provider url to call
+      facebook.me().then(data => {
+        console.log('me data:', data);
+        alert('Facebook says your name is:' + data.name + ".\nView browser 'Console Log' for more details");
+        newUser = {
+          userName: data.name,
+          password: "test"
+        };
+        addUser(newUser);
+      })
+      // Retrieves user data from OAuth provider by using #get() and
+      // OAuth provider url
+      // facebook.get('/v2.5/me?fields=name,first_name,last_name,email,gender,location,locale,work,languages,birthday,relationship_status,hometown,picture').then(data => {
+      //   console.log('self data:', data);
+        
+      // })
+    });
+    console.log(newUser.userName, " in facebook button");
+  });
+  $('#google-button').on('click', function () {
+    console.log("google clicked");
+    // Initialize with your OAuth.io app public key
+    OAuth.initialize('vjlnKXXv_pB-M71yxzZp5Z5hB-k')
+
+    // // Use popup for oauth
+    OAuth.popup('google_plus').then(google => {
+      console.log('google:', google);
+      //   // Prompts 'welcome' message with User's email on successful login
+      //   // #me() is a convenient method to retrieve user data without requiring you
+      //   // to know which OAuth provider url to call
+      google.me().then(data => {
+        console.log('me data:', data);
+        alert('Google says your email is:' + data + ".\nView browser 'Console Log' for more details");
+        newUser = {
+          userName: data.name,
+          password: "test"
+        };
+        addUser(newUser);
+      });
+      //   // Retrieves user data from OAuth provider by using #get() and
+      //   // OAuth provider url
+      //google.get('/v2.5/me?fields=name,first_name,last_name,email,gender,location,locale,work,languages,birthday,relationship_status,hometown,picture').then(data => {
+      //    console.log('self data:', data);
+      //})
+    });
   });
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
+  //Log in with Twitter
+  $('#twitter-button').on('click', function () {
+    console.log("twitter clicked");
+    // Initialize with your OAuth.io app public key
+    OAuth.initialize('vjlnKXXv_pB-M71yxzZp5Z5hB-k');
 
-
-//LOGIN//
-
-//Log in with Facebook//
-$('#facebook-button').on('click', function() {
-  // Initialize with your OAuth.io app public key
-  OAuth.initialize('vjlnKXXv_pB-M71yxzZp5Z5hB-k');
-  // Use popup for oauth
-  OAuth.popup('facebook').then(facebook => {
-    console.log('facebook:',facebook);
-    // Prompts 'welcome' message with User's email on successful login
-    // #me() is a convenient method to retrieve user data without requiring you
-    // to know which OAuth provider url to call
-    facebook.me().then(data => {
-      console.log('me data:', data);
-      alert('Facebook says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
-    })
-    // Retrieves user data from OAuth provider by using #get() and
-    // OAuth provider url
-    facebook.get('/v2.5/me?fields=name,first_name,last_name,email,gender,location,locale,work,languages,birthday,relationship_status,hometown,picture').then(data => {
-      console.log('self data:', data);
-    })
+    // Use popup for oauth
+    OAuth.popup('twitter').then(twitter => {
+      console.log('twitter:', twitter);
+      // Prompts 'welcome' message with User's email on successful login
+      // #me() is a convenient method to retrieve user data without requiring you
+      // to know which OAuth provider url to call
+      twitter.me().then(data => {
+        console.log('me data:', data);
+        alert('Twitter says your name is:' + data.name + ".\nView browser 'Console Log' for more details");
+        newUser = {
+          userName: data.name,
+          password: "test"
+        };
+        addUser(newUser);
+      })
+    });
+    loadMenu();
   });
+
 });
-//Log in with google//
-$('.fa-google-plus-square').on('click', function() {
-  // console.log("google clicked");
-  // // Initialize with your OAuth.io app public key
-  // OAuth.initialize('vjlnKXXv_pB-M71yxzZp5Z5hB-k');
-  // // Use popup for oauth
-  // OAuth.popup('google').then(google => {
-  //   console.log('google:',google);
-  //   // Prompts 'welcome' message with User's email on successful login
-  //   // #me() is a convenient method to retrieve user data without requiring you
-  //   // to know which OAuth provider url to call
-  //   google.me().then(data => {
-  //     console.log('me data:', data);
-  //     alert('Google says your email is:' + data.email + ".\nView browser 'Console Log' for more details");
-  //   })
-    // Retrieves user data from OAuth provider by using #get() and
-    // OAuth provider url
-    // google.get('/v2.5/me?fields=name,first_name,last_name,email,gender,location,locale,work,languages,birthday,relationship_status,hometown,picture').then(data => {
-    //   console.log('self data:', data);
-    // })
-  //});
-});
+
+function loadMenu() {
+  $.get("/menu", {
+    type: "GET"
+  });
+
+}
+
+function addUser(user) {
+  $.post("/api/users/", user, function () {
+    window.location.href = "/menu";
+  });
+}
