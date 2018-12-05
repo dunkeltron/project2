7a9c025ab0e8934ac936ebddfbe8e2e218c6b93b
Ian Dunkelberg <idunkelberg@gmail.com>
2018-12-04 20:33:50 -0800

    added sequelize models for photo and events in photo.js and event.js

---
+++ models/photo.js
@@ -0,0 +1,25 @@
+module.exports = function (sequelize, DataTypes) {
+  var Photo = sequelize.define("Photo", {
+    userId: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    eventId: {
+      type: Datatypes.STRING,
+      allowNull:false
+    },
+    photoName: {
+      type: Datatypes.STRING
+      allowNull:false
+    },
+    score:{
+        type: DataTypes.INTEGER,
+        allowNull:true
+    },
+    photoLink:{
+        type: DataTypes.STRING,
+        allowNull:false
+    }
+  });
+  return Photo;
+};
