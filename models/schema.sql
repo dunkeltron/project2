c6d130134ed306e148e10ef432a1822ee9eca27b
Ian Dunkelberg <dunkeltron@users.noreply.github.com>
2018-12-08 09:45:19 -0800

    Update schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -1,9 +1,3 @@
 
 DROP DATABASE IF EXISTS photo_db;
 CREATE DATABASE photo_db;
-USE photo_db;
-CREATE TABLE user (
-  `userId` INT NOT NULL AUTO_INCREMENT,
-  `password` VARCHAR(45) NULL,
-  `userName` VARCHAR(45) NULL,
-  PRIMARY KEY (`userId`));
